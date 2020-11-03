b46eebe71c960e3c133fb5a8579296922d6bb1f7
Heum <rhksgma258@gmail.com>
2020-11-04 00:06:29 +0900

    2020.11.04

---
+++ BJ/bj11725.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int arr[100000] = {0,};
+vector<int> tree[100001];
+void func(int cnt){
+    if(tree[cnt].size()==0) return;
+    for(int i=0; i< tree[cnt].size(); i++){
+        int index = tree[cnt][i];
+        //cout << cnt <<" "<<index << endl;
+        if(arr[index]==0){
+            arr[index] = cnt;
+            func(index);
+        }
+    }
+}
+
+
+int main(void){
+    int N;
+    cin >> N;
+    for(int i=0; i<N-1; i++){
+        int a, b;
+        cin >> a >> b;
+        tree[a].push_back(b);
+        tree[b].push_back(a);
+    }
+    arr[1] = 1;
+    func(1);
+    for(int i=2; i<=N; i++){
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
