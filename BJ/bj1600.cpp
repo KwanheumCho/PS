823a1844bcea5874264b803657cdc8a02202230d
Heum <rhksgma258@gmail.com>
2020-11-11 08:32:51 +0900

    2020.11.11

--- BJ/bj1600.cpp
+++ BJ/bj1600.cpp
@@ -1,89 +1,57 @@
 #include <iostream>
-
+#include <queue>
 using namespace std;
 
 int K, W, H;
-int map_[201][201];
-int dp_[201][201];
+int map_[200][200];
 int result = 200*200;
 
 
-
-int dx[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
-int dy[8] = {-1, -2, -2, -1, 1, 2, 2, 1};
-
-int ddx[4] = {-1, 0, 1, 0};
-int ddy[4] = {0, -1, 0, 1};
-
-void mongkey_move(int y, int x, int cnt){
-    //cout << "mon : " << y <<" "<< x << endl;
-    if( y == H-1 && x == W-1){
-        if(cnt < result){
-            result = cnt;
-        }
-        return;
-    }
-    if(cnt > result) return;
-
-    for(int i=0; i<4; i++){
-        int nx = x + ddx[i];
-        int ny = y + ddy[i];
-        if( nx>=0 && nx<W && ny>=0 && ny<H){
-            if(map_[ny][nx]==0) mongkey_move(ny,nx, cnt+1);
-        }
-    }
-}
-
-void horse_move(int y, int x, int cnt){
-    if(cnt <= K){
-        //한칸씩 이동
-        mongkey_move(y , x, cnt);
-        //cout << "hor : " << y <<" "<< x << endl;
-        
-    }
-    if(cnt == K) return;
-    for(int i=0; i<8; i++){
-        //말의 움직임처럼 이동
-        int nx = x+dx[i];
-        int ny = y+dy[i];
-        if( nx>=0 && nx<W && ny>=0 && ny<H){
-            if(map_[ny][nx]==0) {
-                horse_move(ny, nx, cnt+1);
-            }
-        }
-    }
-}
-/*
 int dx[12] = {-2, -1, 1, 2, 2, 1, -1, -2, -1, 0, 1, 0};
 int dy[12] = {-1, -2, -2, -1, 1, 2, 2, 1, 0, -1, 0, 1};
+queue< pair< pair<int , int > , pair<int, int> > > q; // y좌표 x좌표 k값 cnt값
+bool chk_[200][200][31] = {false,};
 
-void move_func(int y, int x, int k, int cnt){
-    if(cnt > result) return;
-    if( y == H-1 && x == W-1){
-        if(cnt < result){
-            result = cnt;
+void move_func(int x, int y, int k, int cnt){
+    q.push( make_pair( make_pair(x, y) , make_pair(k, cnt) )  );
+    chk_[x][y][k] = true;
+    while(!q.empty()){
+        x = q.front().first.first;
+        y = q.front().first.second;
+        k = q.front().second.first;
+        cnt = q.front().second.second;
+        //cout << "x: "<< x<< "y: "<<y<<" s: "<<q.size() << endl;
+        q.pop();
+        if(x == W-1 && y == H-1){
+            if(cnt < result) result = cnt;
+            return;
         }
-        return;
-    }
-
-    for(int i=0; i<12; i++){
-        //말의 움직임처럼 이동
-        int nx = x+dx[i];
-        int ny = y+dy[i];
-        if( nx>=0 && nx<W && ny>=0 && ny<H){
-            if(map_[ny][nx]==0) {
-                if(i<8 && k<K) move_func(ny, nx, k+1, cnt+1);
-                if(i>=8) move_func(ny, nx, k, cnt+1);
+        for(int i=0; i<12; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx >= 0 && nx < W && ny >=0 && ny < H){
+                if(map_[ny][nx] == 0){
+                    if(i<8 && k < K && !chk_[ny][nx][k+1]){
+                        //q push
+                        chk_[ny][nx][k+1] = true;
+                        q.push( make_pair( make_pair(nx, ny) , make_pair(k+1, cnt+1) )  );
+                    }
+                    else if(i>=8 && !chk_[ny][nx][k]){
+                        //q push
+                        chk_[ny][nx][k] = true;
+                        q.push( make_pair( make_pair(nx, ny) , make_pair(k, cnt+1) )  );
+                    }
+                }
             }
-        }
-    }
+        }//for
+    }//while
+    return;
+
 }
-*/
+
 
 int main(void){
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-
+    
     cin >> K; // K번 움직일 수 있다
     cin >> W >> H; // 가로길이 W , 세로길이 H [h][w]
     for(int i=0; i<H; i++){
@@ -93,17 +61,8 @@ int main(void){
     }
     //시작점 : 맨왼쪽위 (0,0)
     //도착점 : 맨오른쪽아래 (H-1,W-1)
-    
-    horse_move(0 , 0, 0);
-    if(result == 200*200){
-        cout << result << endl;
-    } 
-    else cout << result << endl;
-    
-    /*
     move_func(0, 0, 0, 0);
-    if(result == 200*200) cout << -1 << endl;
-    else cout << result << '\n';
-    */
+    if(result!= 200*200) cout << result << endl;
+    else cout << -1 << endl;
     return 0;
 }
